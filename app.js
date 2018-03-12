0a49ff4e19d97f4007affb7efac69dec474c6c49
Molly Seeley <mseeley89@gmail.com>
2018-03-12 12:07:11 -0400

    finnished prompt 3

--- app.js
+++ app.js
@@ -4,17 +4,30 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 var path = require('path');
+var models = require('./models');
 
+var route = models.Page.urlTitle;
 app.use(morgan('dev'));
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '/public')));
+//db
+// get [page.urlTitle]() {return 'wiki' + page.urlTitle ;}
 
-var server = app.listen(3000, function () {
-    console.log('listening')
-});
+
+//syncing/integrating tables w/app + server
+models.db.sync({force:true})
+.then(()=>{
+    console.log('Page Table created!');
+    app.listen(3000, ()=>{
+        console.log('Server is listening on port 3000!');
+    })
+})
+.catch(console.error.bind(console));
+//why are we binding here? 
+
 
 const env = nunjucks.configure('views', {noCache: true});
 
--- models/index.js
+++ models/index.js
@@ -1,2 +1,49 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false
+});
+//quiets the terminal logging
+
+
+const Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false},
+    urlTitle: {
+        type: Sequelize.STRING,
+        get(){
+            return '/wiki/' + this.getDataValue('page');
+        },
+        allowNull: false},
+        //what url name to be passed second
+    content: {
+        type: Sequelize.TEXT, 
+        allowNull: false},
+    status: {
+        type: Sequelize.ENUM('open', 'closed')},
+    date: {
+        type: Sequelize.TEXT,
+        defaultValue: Sequelize.NOW
+    }
+})
+
+const User = db.define('user', {
+    name: {
+        type: Sequelize.STRING, 
+        allowNull: false},
+    email: {
+        type: Sequelize.STRING, 
+        allowNull: false,
+        validate: {
+            isEmail:true
+        }
+    }
+})
+//what is: sequelize.NOW?
+
+
+module.exports = {
+    db: db,
+    Page: Page,
+    User: User
+  };
